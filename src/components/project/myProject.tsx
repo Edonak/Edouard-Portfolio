047524413b13715ed18c907b6cec4adb5e59831c
Edonak <edouardnakasila69@gmail.com>
2024-08-17 22:04:10 +0100

    responsive project

--- src/components/project/myProject.tsx
+++ src/components/project/myProject.tsx
@@ -1,35 +1,32 @@
-
-import ButtonGeneral from '../primaryComponents/buttonGeneral'
+import ButtonGeneral from "../primaryComponents/buttonGeneral";
 
 export default function MyProject() {
   return (
     <section
-    className="mx-auto my-auto py-10w-[100%] h-[70vh] bg-slate-100"
-    id="myprojects"
-  >
-    <div className="mx-auto my-auto">
-      <p className="text-center uppercase font-bold text-3xl text-blue-800 pt-8">
-       
-        ** Mes Projets
-      </p>
-      <h2 className="text-center font-semibold text-xl mb-8">
-        Les projets sur lesquels j'ai travaillé
-      </h2>
-    </div>
-    <div className=" flex flex-wrap w-[80%] h-[45vh] justify-center mx-auto my-auto py-5 gap-20">
-      <div className=" project w-[25%] bg-slate-50 border-solid rounded-3xl shadow-lg  ">
-         <p className='text-center'>Habiteo</p>
+      className="mx-auto my-auto py-5 bg-slate-100"
+      id="myProjects"
+    >
+      <div className="mx-auto my-auto">
+        <p className="text-center uppercase font-bold text-3xl text-blue-800 pt-8">
+          ** Mes Projets
+        </p>
+        <h2 className="text-center font-semibold text-xl mb-8">
+          Les projets sur lesquels j'ai travaillé
+        </h2>
       </div>
-      <div className=" project w-[25%] bg-slate-50 border-solid rounded-3xl shadow-lg">
-        <p className='text-center'>Megalo</p>
-          </div>
-     
-      <div className=" project w-[25%]  bg-slate-50 border-solid rounded-3xl shadow-lg">
-         <p className='text-center'>Gang</p>
-    </div>
-    </div>
-    <ButtonGeneral value='Tout voir' className='text-center' />
-  </section>
-  )
-}
+      <div className="myProjectsContainer mx-auto my-auto py-5">
+        <div className=" project bg-slate-50 border-solid rounded-3xl shadow-lg  ">
+          <p className="text-center">Habiteo</p>
+        </div>
+        <div className=" project bg-slate-50 border-solid rounded-3xl shadow-lg">
+          <p className="text-center">Megalo</p>
+        </div>
 
+        <div className=" project bg-slate-50 border-solid rounded-3xl shadow-lg">
+          <p className="text-center">Gang</p>
+        </div>
+      </div>
+      <a href="" className="buttonVoirPlus"> Voir Plus</a>
+    </section>
+  );
+}
