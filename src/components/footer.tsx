4f63cda07c5397dad4c60c299ae64a29cfaca5ef
Edonak <edouardnakasila69@gmail.com>
2024-08-17 08:45:15 +0100

    responsive footer finish

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,14 +2,14 @@ import Avatar from "./primaryComponents/avatar";
 
 export default function Footer() {
   return (
-    <footer className="text-center">
-      <div className="footer-avatar">
-        <figure className=" h-[20vh]  mx-auto my-auto">
+    <footer className="mx-auto my-auto text-center">
+      <div>
+        <figure className="mx-auto my-auto">
           <Avatar />
         </figure>
         <q className="font-bold"> Le plus difficile est la décision d'agir, le reste n'est que ténacité</q>
       </div>
-      <div className="w-[20%] mt-8 flex gap-2 mx-auto my-auto">
+      <div className=" mt-8 gap-2 mx-auto my-auto footer-bottom">
         <h5 className="font-bold">Nakasila Edouard</h5>
         <p>© 2024 Tout droit réservé</p>
       </div>
