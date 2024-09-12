e8e35e1f2ec3534e7a893dff14dbfd58abc40cf8
Edonak <edouardnakasila69@gmail.com>
2024-09-12 15:27:49 +0100

    mis à jour du composant contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -31,93 +31,53 @@ const ContactForm: React.FC<ContactFormProps> = () => {
     console.log(formData);
   };
   return (
-    <section className=" h-[90vh] bg-slate-400 containerFormulaire">
-      <div className="pt-10">
-        <div className="">
-          <div className="">
-            <div className="bg-white relative flex shadow-lg w-[50%]  mx-auto my-auto h-[70vh] rounded-t-3xl  rounded-br-none">
-              <div className="">
-                <form onSubmit={handleSubmit} className="formulaire rounded-none border-b border-gray-300 p-6 pb-9 pt-20 pl-10 pr-40 ">
-                    <h3 className="text-black text-4xl tracking-wide font-bold mb-4">
-                      Me Contacter
-                    </h3>
-                    <div>
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        className="rounded-none border-b border-gray-300 p-3 w-[100%] outline-none"
-                        placeholder="Votre nom"
-                        value={formData.name}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="rounded-none border-b border-gray-300 p-3 w-[100%] outline-none"
-                        placeholder="Votre adresse email"
-                        value={formData.email}
-                        onChange={handleChange}
-                      />
-                    </div>
-                    <div>
-                      <textarea
-                        name="message"
-                        id="message"
-                        className="rounded-none border-b border-gray-300 p-3  w-[100%] outline-none"
-                        placeholder="Votre message"
-                        value={formData.message}
-                        onChange={handleChange}
-                      ></textarea>
-                    </div>
-                    <ButtonGeneral
-                      value="Envoyer"
-                      className="contact_form_submit w-[100%] text-white hover:bg-transparent hover:text-blue-800 hover:border-blue-800 font-semibold  py-3 px-4 mt-8 border border-white rounded-full"
-                    />
-                </form>
-
-              </div>
-              <div className="contact rounded-tr-3xl w-[40%] bg-black pt-14 pl-4">
-                <h3 className="tracking-wide pb-6 text-center font-bold uppercase">Information Contact</h3>
-                <div className="d-flex info_single align-items-center">
-                  <i className="fas fa-headset mr-4"></i>
-                  <p>+243 822 874 347</p>
-                  <p>+243 844 060 541</p>
-                </div>
-                <div className="d-flex info_single align-items-center">
-                  <i className="fas fa-envelope-open-text"></i>
-                  <span>nakasilaedouard@gmail.com</span>
-                </div>
-                <div className="d-flex info_single align-items-center">
-                  <i className="fas fa-map-marked-alt"></i>
-                  <span>
-                    17B Bis, Viaza, Q/ Viaza Matete Kinshasa RDC
-                  </span>
-                </div>
-                <div className="">
-                  <div className="social-icons flex gap-7">
-                    <a
-                      href="https://github.com/votre-nom-github"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/votre-nom-linkedin"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                    </a>
-                    <a href="mailto:votre-email@email.com">
-                    </a>
-                  </div>
-                </div>
-              </div>
+    <section className="bg-slate-200 containerFormulaire">
+      <div className="formContainerDoc">
+        <div className="formContainer bg-white relative flex shadow-lg mx-auto my-auto">
+          <form onSubmit={handleSubmit} className="formulaire rounded-none border-b border-gray-300 pl-10 pr-40 ">
+            <h3 className="text-black text-2xl tracking-wide font-bold mb-4">
+              Me Contacter
+            </h3>
+            <div>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="rounded-none border-b border-gray-300 p-3 w-[100%] outline-none"
+                placeholder="Votre nom"
+                value={formData.name}
+                onChange={handleChange}
+              />
             </div>
-          </div>
+            <div>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="rounded-none border-b border-gray-300 p-3 w-[100%] outline-none"
+                placeholder="Votre adresse email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <textarea
+                name="message"
+                id="message"
+                className="rounded-none border-b border-gray-300 p-3  w-[100%] outline-none"
+                placeholder="Votre message"
+                value={formData.message}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <ButtonGeneral
+              value="Envoyer"
+              className="contact_form_submit w-[100%] text-white hover:bg-transparent hover:text-blue-800 hover:border-blue-800 font-semibold  py-3 px-4 mt-8 border border-white rounded-full"
+            />
+          </form>
+          <figure className="contact bg-black ">
+            <img src="./../../public/images/nak2.jpg" alt=""/>
+          </figure>
         </div>
       </div>
     </section>
